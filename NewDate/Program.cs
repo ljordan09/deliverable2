48414e183fa4fa9fd99aafb10f18670c60a0cce2
leonel contreras <you@example.com>
2017-10-10 13:15:00 -0400

    deliverable2

---
+++ NewDate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateDiff();
+            Console.ReadLine();
+           
+
+            
+
+        }
+
+        public static void DateDiff()
+        {
+
+            Console.WriteLine("Enter any date: ");
+            DateTime firstDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another date: ");
+            DateTime secondDate = DateTime.Parse(Console.ReadLine());
+
+            TimeSpan difference = firstDate - secondDate;
+            
+            Console.WriteLine("Days : " + difference.TotalDays.ToString());
+            Console.WriteLine("Hours : " + difference.TotalHours.ToString());
+            Console.WriteLine("Minutes : " + difference.TotalMinutes.ToString());
+           
+
+        }
+      
+    }
+
+    
+}

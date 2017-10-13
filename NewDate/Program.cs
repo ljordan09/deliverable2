6a16643fb542851db3529ebc26161ceb61e61f3b
leonel contreras <you@example.com>
2017-10-13 00:52:53 -0400

    second commit

--- NewDate/Program.cs
+++ NewDate/Program.cs
@@ -13,19 +13,21 @@ namespace NewDate
 
             Dates();
             Console.ReadLine();         
-
+            
         }
 
         public static void Dates()
         {
-
-            Console.WriteLine("Enter any date: ");
+            //requests user input
+            Console.WriteLine("Enter date and time in any format (eg. mm/dd/yyy and 12:00) : ");
             DateTime firstDate = DateTime.Parse(Console.ReadLine());
-            Console.WriteLine("Enter another date: ");
+            Console.WriteLine("Enter another date and time: ");
             DateTime secondDate = DateTime.Parse(Console.ReadLine());
-
-            TimeSpan diff = (firstDate - secondDate).Duration();
             
+            //calculates the time duration between the two user inputs
+            TimeSpan diff = (firstDate - secondDate).Duration();
+
+            //displays the time duration in days, hours, and minutes
             Console.WriteLine("Days : " + diff.TotalDays.ToString());
             Console.WriteLine("Hours : " + diff.TotalHours.ToString());
             Console.WriteLine("Minutes : " + diff.TotalMinutes.ToString());
